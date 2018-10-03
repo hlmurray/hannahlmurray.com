ebe2f0381cab4c6ed57dbd438e6d7d145943d2bc
hlmurray91 <hlmurray91@gmail.com>
2018-10-03 08:51:25 -0400

    Create SocialNav component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
 import React, { Component } from 'react'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Container, Title, Content, Link, Headshot, Nav } from './styling'
+import { Container, Title, Content, Link, Headshot } from './styling'
+import SocialNav from './components/SocialNav'
 import headshot from './hlm.jpg'
-
-library.add(fab)
-
-class SocialNav extends React.Component {
-    render() {
-        const icons = [
-            {name: 'facebook-f', url: 'https://www.facebook.com/hlmurray91'},
-            {name: 'instagram', url: 'https://www.instagram.com/hannahlmurray/'},
-            {name: 'twitter', url: 'https://twitter.com/hlmurray'},
-            {name: 'linkedin-in', url: 'https://www.linkedin.com/in/hlmurray/'},
-            {name: 'github', url: 'https://github.com/hlmurray'}
-        ]
-
-        const socialList = icons.map((icon) => {
-            return <li key={icon.name}><Link href={icon.url} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', `${icon.name}`]} /></Link></li>
-    	})
-
-        return  <Nav>{ socialList }</Nav>
-    }
-}
 
 class App extends Component {
     render() {
