76da6a3cc09b592f0dd5d7d5ab8c7b750136891e
hlmurray91 <hlmurray91@gmail.com>
2020-03-18 20:47:53 -0400

    Update job title

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
         <Container>
             <Title>I&rsquo;m Hannah</Title>
             <Headshot src={headshot} alt="hannahlmurray" />
-            <Content>I&rsquo;m a Senior Front End Engineer based out of Brooklyn, NY. Currently working at <Link href="https://casper.com" target="_blank" rel="noopener noreferrer">Casper</Link>, previously at <Link href="https://mmlafleur.com" target="_blank" rel="noopener noreferrer">MM.LaFleur</Link>, <Link href="https://thecharlesnyc.com" target="_blank" rel="noopener noreferrer">The Charles</Link> and Time Inc. Originally from South Carolina, and studied Computer Science at the College of Charleston. Lover of running, cooking, Saturday Night Live, and Bravo.</Content>
+            <Content>I&rsquo;m an Engineering Manager based out of Brooklyn, NY. Currently working at <Link href="https://casper.com" target="_blank" rel="noopener noreferrer">Casper</Link>, previously at <Link href="https://mmlafleur.com" target="_blank" rel="noopener noreferrer">MM.LaFleur</Link>, <Link href="https://thecharlesnyc.com" target="_blank" rel="noopener noreferrer">The Charles</Link> and Time Inc. Originally from South Carolina, and studied Computer Science at the College of Charleston. Lover of running, cooking, Saturday Night Live, and Bravo.</Content>
             <SocialNav />
         </Container>
     )
